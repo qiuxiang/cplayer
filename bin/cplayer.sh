d7c088c81ad9a64ac12a9df085b0ab0b654c747f
Qiu Xiang <xiang.qiu@qq.com>
2015-03-11 18:30:44 +0800

    添加 zenity

--- bin/cplayer.sh
+++ bin/cplayer.sh
@@ -14,7 +14,15 @@ get_raw_urls() {
   $PARSE "$(nodejs -e "
     function flvout (html) { console.log(html) }
     $(curl -s -H "referer: http://flv.cn" \
-        https://www.flvxz.com/getFlv.php?url=$($ENCODE "$1") | grep -o "eval.*));")")"
+        https://www.flvxz.com/getFlv.php?url=$($ENCODE "$1") \
+      | grep -o "eval.*));")")"
 }
 
-get_raw_urls "http://v.youku.com/v_show/id_XOTA4MjY1ODI4.html"
+implode() {
+  echo "$1"
+}
+
+urls=$(get_raw_urls "http://v.youku.com/v_show/id_XOTA4MjY1ODI4.html")
+qualitys=$(echo $urls | jq ".fragments | keys")
+choice=$(zenity --height="320" --list --column="视频品质" ${qualitys//[\[\]\",]/})
+echo ${choice#*|}
